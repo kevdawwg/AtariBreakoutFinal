369103f8b8a8aa956dc506f88d9bb3bd794a8eba
KaranC7258 <99352551+KaranC7258@users.noreply.github.com>
2022-05-16 09:05:31 -0700

    Paddle glitch fixed

--- src/Game.java
+++ src/Game.java
@@ -138,7 +138,7 @@ public class Game {
             player.play(2, 0);
             return;
         }
-        if (ball.getRect().y + ball.getRect().height >= Board.HEIGHT) {
+        if (ball.getRect().y + ball.getRect().height >= 520) {
             System.out.println("hit the bottom");
             player.play(1, 0);
             try {
