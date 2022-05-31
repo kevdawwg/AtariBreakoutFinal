7ead44254b28b99570fd8363c211b5f78e3a8bb4
Brandon Yao <ahoynodnarb@gmail.com>
2022-05-30 18:21:42 -0700

    Optimizations

--- src/Game.java
+++ src/Game.java
@@ -1,65 +1,59 @@
 import java.util.*;
 import javax.imageio.ImageIO;
-import javax.swing.JButton;
-import javax.swing.KeyStroke;
-import java.awt.event.KeyEvent;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-// import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class Game {
-    private Paddle paddle;
+    public Point panelLocation;
+    public Paddle paddle;
     private ArrayList<GameComponent> bricks;
     private ArrayList<Integer> actions;
     private ArrayList<Integer> removed;
-    private Image[] imgs;
+    private BufferedImage[] imgs;
     private Ball ball;
     private int score;
     private int lives;
     private SoundPlayer player;
-    private Image background = null;
+    private BufferedImage background = null;
     private BufferedImage gameOverImg;
     private BufferedImage gameStartImg;
     private int brickRespawns = 0;
     private GameComponent startScreen;
     private boolean gameStart = false;
     public static final int START_WIDTH = 200;
-    public static final int START_HEIGHT = 150; 
+    public static final int START_HEIGHT = 150;
     public static final int BRICK_WIDTH = 70;
     public static final int BRICK_HEIGHT = 40;
     public static final int PADDLE_WIDTH = 100;
     public static final int PADDLE_HEIGHT = 20;
-    public static final int BALL_WIDTH = 10;
-    public static final int BALL_HEIGHT = 10;
+    public static final int BALL_WIDTH = 20;
+    public static final int BALL_HEIGHT = 20;
     public static final int BRICK_SPACE = 30;
     public static final int SPEED_CAP = 25;
     public static final int SPEED_INCREMENT = 1;
     public static final Font FONT_LARGE = new Font("Times New Roman", Font.BOLD, 40);
-    private Board board;
 
-    public Game(Board board) {
-        this.board = board;
+    public Game() {
         bricks = new ArrayList<>();
         removed = new ArrayList<>();
         actions = new ArrayList<>();
-        imgs = new Image[4];
+        imgs = new BufferedImage[4];
         paddle = new Paddle(300, 500, PADDLE_WIDTH, PADDLE_HEIGHT);
         ball = new Ball(300, 450, BALL_WIDTH, BALL_HEIGHT, 10, -10);
         lives = 1;
         score = 0;
         player = new SoundPlayer();
-        player.play(0, 66000000); 
+        player.play(0, 66000000);
         player.loop();
         loadImages();
         respawnBricks();
     }
 
     public void moveObjects() {
-        paddle.mouseMove();
+        // paddle.mouseMove(panelLocation);
         ball.move();
         // if (actions.size() > 0) {
         // paddle.buttonMove(actions.remove(0));
@@ -72,18 +66,20 @@ public class Game {
         if (actions.size() > 0 && actions.get(0) == 2) {
             gameStart = true;
             actions.remove(0);
-            if(lives<=0){
-                
+            if (lives <= 0) {
                 score = 0;
                 lives = 3;
                 bricks = new ArrayList<>();
                 respawnBricks();
-                try {Thread.sleep(1000);}
-                catch (Exception e) {e.printStackTrace();}
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
                 ball.setDx(10);
                 ball.setDy(10);
             }
-        } 
+        }
 
         if (lives == 0 && actions.size() > 0 && actions.get(0) == 3) {
             System.out.println(actions.size());
@@ -92,8 +88,7 @@ public class Game {
                 System.out.println("exiting");
                 System.out.println("After " + actions.size());
                 System.exit(0);
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -104,9 +99,9 @@ public class Game {
             checkCollisions();
         }
     }
-    
+
     public void loadImages() {
-        try {    
+        try {
             gameOverImg = ImageIO.read(new File("./images/gameOver.png"));
             gameStartImg = ImageIO.read(new File("./images/retroBackground.jpeg"));
             background = ImageIO.read(new File("./images/space-background.png"));
@@ -114,14 +109,13 @@ public class Game {
             imgs[1] = ImageIO.read(new File("./images/yellow_brick.png"));
             imgs[2] = ImageIO.read(new File("./images/green_brick.png"));
             imgs[3] = ImageIO.read(new File("./images/red_brick.png"));
-        }   
-        catch(IOException e){e.printStackTrace();}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
+
     public void drawBackground(Graphics g) {
-        if (!gameStart) g.drawImage(gameStartImg, 0, 0, 750, 600, null);
-        else g.drawImage(background, 0, 0, 750, 600, null);
-        if(lives==0){
+        if (lives == 0) {
             g.drawImage(gameOverImg, 0, 0, 750, 600, null);
             g.setColor(Color.RED);
             g.setFont(FONT_LARGE);
@@ -129,12 +123,17 @@ public class Game {
             g.setColor(Color.GREEN);
             g.setFont(FONT_LARGE);
             g.drawString("Space - play again", 50, 450);
+            return;
         }
+        if (!gameStart)
+            g.drawImage(gameStartImg, 0, 0, 750, 600, null);
+        else
+            g.drawImage(background, 0, 0, 750, 600, null);
     }
 
     public void drawStuff(Graphics g) {
         drawBackground(g);
-        if(lives<=0){
+        if (lives <= 0) {
             gameStart = false;
             ball.setDx(0);
             ball.setDy(0);
@@ -146,26 +145,20 @@ public class Game {
             g.drawString("Press space", 270, 450);
         }
         if (gameStart) {
-            paddle.draw(g, Color.GREEN);
             paddle.updatePaddle(g);
-            ball.draw(g, Color.WHITE);
             ball.updateBall(g);
-            for(int i = 0; i < bricks.size(); i++){
+            for (int i = 0; i < bricks.size(); i++) {
                 bricks.get(i).draw(g);
                 int height = bricks.get(i).getRect().y;
-                Image img = null;
-                if(height == (BRICK_HEIGHT + BRICK_SPACE) + 30) img = imgs[0]; 
-                else if(height == 2 * (BRICK_HEIGHT + BRICK_SPACE) + 30) img = imgs[1];
-                else if(height == 3 * (BRICK_HEIGHT + BRICK_SPACE) + 30) img = imgs[2]; 
-                else img = imgs[3]; 
-                g.drawImage(img, bricks.get(i).getRect().x, bricks.get(i).getRect().y, BRICK_WIDTH, BRICK_HEIGHT,null);
+                int index = height / ((BRICK_HEIGHT + BRICK_SPACE) + 30);
+                Image img = imgs[index];
+                g.drawImage(img, bricks.get(i).getRect().x, bricks.get(i).getRect().y, BRICK_WIDTH, BRICK_HEIGHT, null);
             }
             g.setColor(Color.WHITE);
             g.drawString("Lives: " + lives, 550, 550);
             g.drawString("Score: " + score, 650, 550);
         }
     }
-
 
     public void respawnBricks() {
         if (bricks.size() == 0) {
@@ -177,17 +170,17 @@ public class Game {
             try {
                 Thread.sleep(2000);
                 player.play(3, 0);
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            if (brickRespawns > 0) player.play(3, 0);
-            Color color = null;
+            if (brickRespawns > 0)
+                player.play(3, 0);
             Color[] colors = new Color[] { Color.RED, Color.YELLOW, Color.GREEN, Color.CYAN };
             for (int r = 0; r < 4; r++) {
+                Color color = colors[r];
                 for (int c = 0; c < 10; c++) {
-                    color = colors[r];
-                     bricks.add(new GameComponent(c * (BRICK_WIDTH + 5), r * (BRICK_HEIGHT + BRICK_SPACE) + 30, BRICK_WIDTH, BRICK_HEIGHT, color));
+                    bricks.add(new GameComponent(c * (BRICK_WIDTH + 5), r * (BRICK_HEIGHT + BRICK_SPACE) + 30,
+                            BRICK_WIDTH, BRICK_HEIGHT, color));
                 }
             }
         }
@@ -208,7 +201,6 @@ public class Game {
             player.play(1, 0);
             try {
                 lives--;
-                board.repaint();
                 Thread.sleep(3000);
                 int randX = ((int) (Math.random() * Board.WIDTH - 99)) + 100;
                 double rand = Math.random();
@@ -268,7 +260,7 @@ public class Game {
     }
 
     public void spaceHit(ActionEvent e) {
-        actions.add(2); 
+        actions.add(2);
     }
 
     public void escapeHit(ActionEvent e) {
--- src/Paddle.java
+++ src/Paddle.java
@@ -1,82 +1,49 @@
 import java.awt.*;
-import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class Paddle extends GameComponent{
+public class Paddle extends GameComponent {
+    private BufferedImage image;
 
     public Paddle(int x, int y, int width, int height) {
         super(x, y, width, height, 0, 0);
+        try {
+            image = ImageIO.read(new File("./images/paddle.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public int getX(){
-        return getX();
-    }
-
-    public void mouseMove() {
-        int x = (int)MouseInfo.getPointerInfo().getLocation().getX();
-        if (x+Game.PADDLE_WIDTH> Board.WIDTH) {
+    public void mouseMove(Point panelLocation) {
+        long mouseX = Math.round(MouseInfo.getPointerInfo().getLocation().getX());
+        int correctedX = (int) Math.round(mouseX - panelLocation.getX());
+        if (correctedX + Game.PADDLE_WIDTH >= Board.WIDTH) {
+            this.getRect().setLocation(Board.WIDTH - Game.PADDLE_WIDTH, 0);
             return;
         }
-        else {
-            this.getRect().setLocation(x, this.getRect().y);
-        }
+        this.getRect().setLocation(correctedX, this.getRect().y);
+        // int x = (int) MouseInfo.getPointerInfo().getLocation().getX();
+        // if (x + Game.PADDLE_WIDTH > Board.WIDTH) {
+        // return;
+        // } else {
+        // this.getRect().setLocation(x, this.getRect().y);
+        // }
     }
 
     public void updatePaddle(Graphics g) {
-        Image img = null;
-        try{img = ImageIO.read(new File("./images/paddle.png"));}
-        catch(IOException e){e.printStackTrace();}
-        g.drawImage(img, getRect().x, getRect().y, getRect().width, getRect().height, null);
+        g.drawImage(image, getRect().x, getRect().y, getRect().width, getRect().height, null);
     }
 
     public void buttonMove(int i) {
-        if (this.getRect().x+100 + 20 > Board.WIDTH || this.getRect().x - 20 < 0) {
+        if (this.getRect().x + 100 + 20 > Board.WIDTH || this.getRect().x - 20 < 0) {
             return;
-        }
-        else if (i == 0) {
-            this.getRect().setLocation(this.getRect().x-20, this.getRect().y);
-        }
-        else if (i == 1) {
-            this.getRect().setLocation(this.getRect().x+20, this.getRect().y);
+        } else if (i == 0) {
+            this.getRect().setLocation(this.getRect().x - 20, this.getRect().y);
+        } else if (i == 1) {
+            this.getRect().setLocation(this.getRect().x + 20, this.getRect().y);
         }
     }
-
-
-    public void keyPressed(KeyEvent e){
-        int x =  getX();
-        int key = e.getKeyCode();
-
-        if (key == KeyEvent.VK_LEFT) {
-
-            
-            this.getRect().setLocation(x, this.getRect().y);
-        }
-
-        if (key == KeyEvent.VK_RIGHT) {
-
-            
-            this.getRect().setLocation(x, this.getRect().y);
-        }
-    }
-
-    public void keyReleased(KeyEvent e) {
-        int x = getX();
-        int key = e.getKeyCode();
-
-        if (key == KeyEvent.VK_LEFT) {
-
-            
-            this.getRect().setLocation(x, this.getRect().y);
-        }
-
-        if (key == KeyEvent.VK_RIGHT) {
-
-            
-            this.getRect().setLocation(x, this.getRect().y);
-        }
-    }
-    
 
 }
